68e20032cb918e3590713a8ed0b3e525429c2d14
Francesco Visonà <visona.francesco@gmail.com>
2023-04-29 15:36:33 +0200

    Created Maven Project

---
+++ src/main/java/it/unipd/dei/xj/App.java
@@ -0,0 +1,13 @@
+package it.unipd.dei.xj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
