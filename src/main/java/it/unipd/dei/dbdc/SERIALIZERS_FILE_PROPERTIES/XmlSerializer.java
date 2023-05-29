a6375412251dc4eb7a29b84105cc789b50828062
boorgoof <federico.31mene@icloud.com>
2023-05-29 17:46:25 +0200

    nienete

--- src/main/java/it/unipd/dei/dbdc/SERIALIZERS_FILE_PROPERTIES/XmlSerializer.java
+++ src/main/java/it/unipd/dei/dbdc/SERIALIZERS_FILE_PROPERTIES/XmlSerializer.java
@@ -9,22 +9,35 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Classe che implementa l'interfaccia "Serializer".
+ * La classe serializza in un file ".xml" una lista di oggetti "Serializable" tramite la libreria Jackson.
+ * @see Serializable
+ */
 public class XmlSerializer implements Serializer {
 
+    /**
+     * Serializza una lista di oggetti Serializable in un file JSON.
+     *
+     * @param objects   Lista di oggetti da serializzare.
+     * @param filePath  Percorso del file JSON di output.
+     * @throws IOException Se si verifica un errore durante la scrittura del file JSON.
+     */
     @Override
     public void serialize(List<Serializable> objects, String filePath) throws IOException {
 
         try {
-            // Creo il mapper
+            // Create an instance of XmlMapper for the serialization
             XmlMapper xmlMapper = new XmlMapper();
-            xmlMapper.enable(SerializationFeature.INDENT_OUTPUT); // Abilita la formattazione indentata
+            xmlMapper.enable(SerializationFeature.INDENT_OUTPUT); // Enable indented formatting
 
-            // Trasformo gli articoli in xml
+            // Transform objects into XML
             String xmlString = xmlMapper.writeValueAsString(objects);
 
-            System.out.println(xmlString);
+            System.out.println(xmlString); // da togliere
 
-            // Li salvo su file
+
+            // Create a new XML file (if it doesn't already exist) and write the XML content to the file
             File xmlOutput = new File(filePath);
             FileWriter fileWriter = new FileWriter(xmlOutput);
             fileWriter.write(xmlString);
