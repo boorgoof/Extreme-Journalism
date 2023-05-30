07ba07495ba7c57b8dcd897048667e82b5db6c1f
boorgoof <federico.31mene@icloud.com>
2023-05-30 17:07:40 +0200

    test

---
+++ src/test/java/it/unipd/dei/dbdc/Deserializers/JsonDeserializerTest.java
@@ -0,0 +1,20 @@
+package it.unipd.dei.dbdc.Deserializers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JsonDeserializerTest {
+
+    @Test
+    public void getFields() {
+    }
+
+    @Test
+    public void setFields() {
+    }
+
+    @Test
+    public void deserialize() {
+    }
+}
---
+++ src/test/java/it/unipd/dei/dbdc/Serializers/CsvSerializerTest.java
@@ -0,0 +1,13 @@
+package it.unipd.dei.dbdc.Serializers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CsvSerializerTest {
+
+    @Test
+    public void serialize() {
+
+    }
+}
