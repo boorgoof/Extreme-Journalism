d5ed4183c00f48cb458783ffd2942b45de8a7d26
boorgoof <federico.31mene@icloud.com>
2023-05-30 23:42:02 +0200

    nuovo deserializer

--- src/main/java/it/unipd/dei/dbdc/Deserializers/JsonDeserializer.java
+++ src/main/java/it/unipd/dei/dbdc/Deserializers/JsonDeserializer.java
@@ -26,6 +26,7 @@ public class JsonDeserializer implements specificDeserializer<Article> {
         else throw new IllegalArgumentException("Deve essere fornito un array di dimensione " + fields.length);
     }
 
+    // Adesso accetto che non ci siano campi. Li mette a null
     @Override
     public List<Article> deserialize(String filePath) throws IOException {
 
@@ -38,14 +39,20 @@ public class JsonDeserializer implements specificDeserializer<Article> {
         List<JsonNode> articleParentNodes = jsonNode.findParents(fields[0]);
 
         for (JsonNode parentNode : articleParentNodes) {
-            Article article = new Article(
-                    parentNode.findValue(fields[0]).asText(),
-                    parentNode.findValue(fields[1]).asText(),
-                    parentNode.findValue(fields[2]).asText(),
-                    parentNode.findValue(fields[3]).asText(),
-                    parentNode.findValue(fields[4]).asText(),
-                    parentNode.findValue(fields[5]).asText()
-            );
+
+            String[] fieldsValues = new String[6];
+            // serve per accettare i casi in cui non esiste la chiave nel file json altrimenti avrei nullPointerException con asText()
+            for(int i=0; i < fields.length; i++){
+                // vedere se è meglio findValue() oppure get()
+                if(parentNode.findValue(fields[i]) != null){
+                    fieldsValues[i] = parentNode.findValue(fields[i]).asText();
+                } else {
+                    fieldsValues[i] = null;
+                }
+
+            }
+
+            Article article = new Article(fieldsValues[0], fieldsValues[1], fieldsValues[2], fieldsValues[3], fieldsValues[4], fieldsValues[5]);
 
             System.out.println(article);
             articles.add(article);
@@ -56,6 +63,48 @@ public class JsonDeserializer implements specificDeserializer<Article> {
     }
 
 }
+
+// cosi accetta campi non presenti. li mette a null
+/*
+@Override
+    public List<Article> deserialize(String filePath) throws IOException {
+
+        List<Article> articles = new ArrayList<>();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(new File(filePath));
+
+        // Trova tutti i nodi che hanno ID dentro (come figlio, quindi sono gli articoli)
+        List<JsonNode> articleParentNodes = jsonNode.findParents(fields[0]);
+
+        for (JsonNode parentNode : articleParentNodes) {
+
+            String[] fieldsValues = new String[6];
+            // serve per accettare i casi in cui non esiste la chiave nel file json altrimenti avrei nullPointerException con asText()
+            for(int i=0; i < fields.length; i++){
+                // vedere se è meglio findValue() oppure get()
+                if(parentNode.findValue(fields[i]) != null){
+                    fieldsValues[i] = parentNode.findValue(fields[0]).asText();
+                } else {
+                    fieldsValues[i] = null;
+                }
+
+            }
+
+            Article article = new Article(fieldsValues[0], fieldsValues[1], fieldsValues[2], fieldsValues[3], fieldsValues[4], fieldsValues[5]);
+
+            System.out.println(article);
+            articles.add(article);
+
+        }
+
+        return articles;
+    }
+
+ */
+
+
+
 
 // idee di miglioramento. deserializzare in modo dinamico
 /*
--- src/test/java/it/unipd/dei/dbdc/Deserializers/JsonDeserializerTest.java
+++ src/test/java/it/unipd/dei/dbdc/Deserializers/JsonDeserializerTest.java
@@ -1,10 +1,12 @@
 package it.unipd.dei.dbdc.Deserializers;
 
+import junit.framework.AssertionFailedError;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonDeserializerTest {
@@ -31,26 +33,88 @@ public class JsonDeserializerTest {
         assertArrayEquals(newFields, fields);
     }
 
-    public void dummyArticles(){
 
+    private static List<Article> createTestArticles() {
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article("ID 1", "URL 1", "Title 1", "Body 1", "Date 1","Source 1"));
+        articles.add(new Article("ID 1", "URL 1", "Title 1", "Body 1", "Date 1","Source 1"));
+        articles.add(new Article("ID 1", "URL 1", "Title 1", "Body 1", "Date 1","Source 1"));
+        return articles;
     }
 
+    // TEST FILE SEMPLICE
     @Test
-    public void deserialize() {
+    public void deserializeSimpleJson() {
 
         JsonDeserializer deserializer = new JsonDeserializer();
+        String[] fileFields = {"id" , "url" , "title" , "body" , "date" , "source"};
+        deserializer.setFields(fileFields);
+
         try {
-            List<Article> articles = deserializer.deserialize("");
+
+            List<Article> articles = deserializer.deserialize("D:\\ingengeria software\\eis-final\\src\\test\\deserializersTest\\Articles1.json"); // mi funziona solo con path preciso
             assertNotNull(articles);
             assertFalse(articles.isEmpty());
-
-            // Assicurati che il numero di articoli deserializzati sia corretto
-           //assertEquals(expectedNumberOfArticles, articles.size());
-            // Effettua ulteriori asserzioni sui dati degli articoli deserializzati se necessario
+            assertEquals(3, articles.size());
+            assertEquals(createTestArticles(), articles);
 
         } catch (IOException e) {
             fail("Errore durante la lettura del file JSON: " + e.getMessage());
         }
 
     }
+    /*
+    // TEST FILE JSON CON COSE ANNIDATE
+    @Test
+    public void deserializeComplexJson() {
+
+        JsonDeserializer deserializer = new JsonDeserializer();
+        String[] fileFields = {"id" , "url" , "title" , "body" , "date" , "source"};
+        deserializer.setFields(fileFields);
+
+        try {
+            List<Article> articles = deserializer.deserialize("D:\\ingengeria software\\eis-final\\src\\test\\deserializersTest\\Articles1.json"); // mi funziona solo con path preciso
+            assertNotNull(articles);
+            assertFalse(articles.isEmpty());
+            assertEquals(3, articles.size());
+            assertEquals(createTestArticles(), articles);
+
+        } catch (IOException e) {
+            fail("Errore durante la lettura del file JSON: " + e.getMessage());
+        }
+
+    }
+
+   */
+    private static List<Article> createTestArticles2() {
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article("ID 1", "URL 1", "Title 1", "Body 1", "Date 1",null));
+        articles.add(new Article("ID 1", "URL 1", "Title 1", "Body 1", "Date 1","Source 1"));
+        articles.add(new Article("ID 1", "URL 1", "Title 1", "Body 1", "Date 1","Source 1"));
+        return articles;
+    }
+
+    // TEST FILE JSON CON CHIAVI MANCANTI
+    @Test
+    public void deserializeNullJson() {
+
+        JsonDeserializer deserializer = new JsonDeserializer();
+        String[] fileFields = {"id" , "url" , "title" , "body" , "date" , "source"};
+        deserializer.setFields(fileFields);
+
+        try {
+            List<Article> articles = deserializer.deserialize("D:\\ingengeria software\\eis-final\\src\\test\\deserializersError\\ArticlesError1.json"); // mi funziona solo con path preciso
+            assertNotNull(articles);
+            assertFalse(articles.isEmpty());
+            assertEquals(3, articles.size());
+            assertEquals(createTestArticles2(), articles);
+
+        } catch (IOException e) {
+            fail("Errore durante la lettura del file JSON: " + e.getMessage());
+        }
+
+    }
+
+    // TEST FILE JSON CON CAMPI NULL
+
 }
